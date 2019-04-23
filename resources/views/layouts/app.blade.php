3a8078c23921e015e1f2560955d3b2f510f6c9bc
XoneFobic <xonefobic@xonecorp.com>
2019-04-23 10:42:43 +0200

    chore: add channel to create thread page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,8 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Left Side Of Navbar -->
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item"><a class="nav-link" href="/threads">All Threads</a></li>
+          <li class="nav-item"><a href="/threads" class="nav-link">All Threads</a></li>
+          <li class="nav-item"><a href="/threads/create" class="nav-link">New Thread</a></li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
                aria-expanded="false">
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -11,17 +11,37 @@
               @csrf
 
               <div class="form-group">
-                <label for="title">Title:</label>
-                <input type="text" name="title" id="title" class="form-control" placeholder="Title" aria-describedby="title">
+                <label for="channel_id">Choose a Channel: <span class="text-danger">*</span></label>
+                <select class="form-control" id="channel_id" name="channel_id" required>
+                  <option value="">Choose a Channel...</option>
+                  @foreach(\App\Channel::all() as $channel)
+                    <option
+                      value="{{ $channel->id }}"{{ intval(old('channel_id')) === $channel->id ? ' selected' : '' }}>{{ $channel->name }}</option>
+                  @endforeach
+                </select>
               </div>
 
               <div class="form-group">
-                <label for="body">Body:</label>
-                <textarea class="form-control" name="body" id="body" rows="8" placeholder="Body"></textarea>
+                <label for="title">Title: <span class="text-danger">*</span></label>
+                <input class="form-control" id="title" name="title" placeholder="Title" required type="text" value="{{ old('title') }}">
               </div>
 
-              <button type="submit" class="btn btn-primary">Publish</button>
+              <div class="form-group">
+                <label for="body">Body: <span class="text-danger">*</span></label>
+                <textarea class="form-control" id="body" name="body" placeholder="Body" required rows="8">{{ old('body ') }}</textarea>
+              </div>
 
+              <div class="form-group">
+                <button class="btn btn-primary" type="submit">Publish</button>
+              </div>
+
+              @if(count($errors))
+                <ul class="alert alert-danger">
+                  @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              @endif
             </form>
           </div>
         </div>
